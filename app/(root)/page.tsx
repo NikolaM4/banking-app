85670126ca5a384233f4ca48431aaf1d92289641
NikolaM4 <nikolamitrovic434@gmail.com>
2024-07-07 20:44:15 +0200

    Starting with UI on home page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,16 @@
 import React from 'react'
+import HeaderBox from '@/components/HeaderBox'
 
 const Home = () => {
-  return <div>Home</div>
+  const loggedIn = { firstName: 'Nikola' }
+
+  return (
+    <section className="home">
+      <div className="home-content">
+        <HeaderBox type="greeting" title="Welcome" user={loggedIn.firstName || 'Guest'} subtext="Access and manage your account and transactions efficiently." />
+      </div>
+    </section>
+  )
 }
 
 export default Home
