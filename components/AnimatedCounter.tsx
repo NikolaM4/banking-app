862d8287a5b20b01cbead53708e6dd20cc450b24
NikolaM4 <nikolamitrovic434@gmail.com>
2024-07-10 20:33:56 +0200

    Created chart and finshed TotalBalanceBox

--- components/AnimatedCounter.tsx
+++ components/AnimatedCounter.tsx
@@ -4,8 +4,8 @@ import CountUp from 'react-countup'
 
 const AnimatedCounter = ({ amount }: { amount: number }) => {
   return (
-    <div>
-      <CountUp end={amount} decimal="," prefix="$" />
+    <div className="w-full">
+      <CountUp end={amount} decimal="," prefix="$" duration={2} decimals={2} />
     </div>
   )
 }
--- components/TotalBalanceBox.tsx
+++ components/TotalBalanceBox.tsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import { formatAmount } from '../lib/utils'
 import AnimatedCounter from './AnimatedCounter'
+import DoughnutChart from './DoughnutChart'
 
 const TotalBalanceBox = ({ accounts = [], totalBanks, totalCurrentBalance }: TotlaBalanceBoxProps) => {
   return (
     <section className="total-balance">
-      <div className="total-balance-chart">{/* <DoughnutChart accounts={accounts} /> */}</div>
+      <div className="total-balance-chart">
+        <DoughnutChart accounts={accounts} />
+      </div>
 
       <div className="flex flex-col gap-6">
         <h2 className="header-2">Bank Accounts: {totalBanks}</h2>
