c8bc514bd316610bcf080c43aa843870166b9825
NikolaM4 <nikolamitrovic434@gmail.com>
2024-07-14 20:47:34 +0200

    Adding shadcn sheet

--- components/MobileNav.tsx
+++ components/MobileNav.tsx
@@ -1,7 +1,20 @@
+import { Sheet, SheetClose, SheetContent, SheetDescription, SheetFooter, SheetHeader, SheetTitle, SheetTrigger } from '@/components/ui/sheet'
 import React from 'react'
 
 const MobileNav = ({ user }: MobileNavProps) => {
-  return <div>MobileNav</div>
+  return (
+    <section>
+      <Sheet>
+        <SheetTrigger>Open</SheetTrigger>
+        <SheetContent>
+          <SheetHeader>
+            <SheetTitle>Are you absolutely sure?</SheetTitle>
+            <SheetDescription>This action cannot be undone. This will permanently delete your account and remove your data from our servers.</SheetDescription>
+          </SheetHeader>
+        </SheetContent>
+      </Sheet>
+    </section>
+  )
 }
 
 export default MobileNav
