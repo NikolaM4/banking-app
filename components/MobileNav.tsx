e771f1e56e6119c51605db4ac554a73838d87ea7
NikolaM4 <nikolamitrovic434@gmail.com>
2024-07-15 21:27:19 +0200

    Working on sidebar for mobile

--- components/MobileNav.tsx
+++ components/MobileNav.tsx
@@ -1,16 +1,38 @@
+'use client'
+
 import { Sheet, SheetClose, SheetContent, SheetDescription, SheetFooter, SheetHeader, SheetTitle, SheetTrigger } from '@/components/ui/sheet'
+import { sidebarLinks } from '@/constants'
+import { cn } from '@/lib/utils'
+import Image from 'next/image'
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 import React from 'react'
 
 const MobileNav = ({ user }: MobileNavProps) => {
+  const pathname = usePathname()
+
   return (
-    <section>
+    <section className="w-fulll max-w-[264px]">
       <Sheet>
-        <SheetTrigger>Open</SheetTrigger>
-        <SheetContent>
-          <SheetHeader>
-            <SheetTitle>Are you absolutely sure?</SheetTitle>
-            <SheetDescription>This action cannot be undone. This will permanently delete your account and remove your data from our servers.</SheetDescription>
-          </SheetHeader>
+        <SheetTrigger>
+          <Image src="/icons/hamburger.svg" width={30} height={30} alt="menu" className="cursor-pointer" />
+        </SheetTrigger>
+        <SheetContent side="left">
+          <Link href="/" className="mb-12 cursor-pointer items-center gap-2 flex">
+            <Image src="/icons/logo.svg" width={34} height={34} alt="Horizon logo" className="size-[24px] max-xl:size-14" />
+            <h1 className="sidebar-logo">Horizon</h1>
+          </Link>
+          {sidebarLinks.map((item) => {
+            const isActive = pathname === item.route || pathname.startsWith(`${item.route}/`)
+            return (
+              <Link href={item.route} key={item.label} className={cn('sidebar-link', { 'bg-bank-gradient': isActive })}>
+                <div className="relative size-6">
+                  <Image src={item.imgURL} alt={item.label} fill className={cn({ 'brightness-[3] invert-0': isActive })} />
+                </div>
+                <p className={cn('sidebar-label', { '!text-white': isActive })}>{item.label}</p>
+              </Link>
+            )
+          })}
         </SheetContent>
       </Sheet>
     </section>
