3a43eb2d648590e2f12f6c907a631bb8450eb64c
NikolaM4 <nikolamitrovic434@gmail.com>
2024-07-18 21:13:50 +0200

    Fixing page.txt and adding RightSidebar to it

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import HeaderBox from '@/components/HeaderBox'
 import TotalBalanceBox from '@/components/TotalBalanceBox'
+import RightSidebar from '@/components/RightSidebar'
 
 const Home = () => {
   const loggedIn = { firstName: 'Nikola' }
@@ -8,9 +9,13 @@ const Home = () => {
   return (
     <section className="home">
       <div className="home-content">
-        <HeaderBox type="greeting" title="Welcome" user={loggedIn.firstName || 'Guest'} subtext="Access and manage your account and transactions efficiently." />
-        <TotalBalanceBox accounts={[]} totalBanks={1} totalCurrentBalance={1250.35} />
+        <header className="home-header">
+          <HeaderBox type="greeting" title="Welcome" user={loggedIn.firstName || 'Guest'} subtext="Access and manage your account and transactions efficiently." />
+          <TotalBalanceBox accounts={[]} totalBanks={1} totalCurrentBalance={1250.35} />
+        </header>
+        RECENT TRANSACTIONS
       </div>
+      <RightSidebar user={loggedIn} transactions={[]} banks={[]} />
     </section>
   )
 }
