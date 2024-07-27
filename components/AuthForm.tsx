f3b08d4d0501ef46a40a47b94ce9a0b2ff5fdac9
NikolaM4 <nikolamitrovic434@gmail.com>
2024-07-27 20:31:36 +0200

    Setting up form

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -7,13 +7,29 @@ import React, { useState } from 'react'
 import { z } from 'zod'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { useForm } from 'react-hook-form'
+import { Button } from '@/components/ui/button'
+import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form'
+import { Input } from '@/components/ui/input'
 
 const formSchema = z.object({
-  username: z.string().min(2).max(50),
+  username: z.string().min(2, {
+    message: 'Username must be at least 2 characters.',
+  }),
 })
 
 const AuthForm = ({ type }: { type: string }) => {
   const [user, setUser] = useState(null)
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: '',
+    },
+  })
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    console.log(values)
+  }
 
   return (
     <section className="auth-form ">
@@ -30,7 +46,31 @@ const AuthForm = ({ type }: { type: string }) => {
           </h1>
         </div>
       </header>
-      {user ? <div className="flex flex-col gap-4">{/* PlaidLink */}</div> : <>FORM</>}
+      {user ? (
+        <div className="flex flex-col gap-4">{/* PlaidLink */}</div>
+      ) : (
+        <>
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+              <FormField
+                control={form.control}
+                name="username"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Username</FormLabel>
+                    <FormControl>
+                      <Input placeholder="shadcn" {...field} />
+                    </FormControl>
+                    <FormDescription>This is your public display name.</FormDescription>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <Button type="submit">Submit</Button>
+            </form>
+          </Form>
+        </>
+      )}
     </section>
   )
 }
