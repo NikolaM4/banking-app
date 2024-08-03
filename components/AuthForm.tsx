8a146b2a2b5f7a8fae8da7de9a2550ac0e71f9cf
NikolaM4 <nikolamitrovic434@gmail.com>
2024-08-03 20:49:23 +0200

    refactor: Improve form layout and button styling in AuthForm

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -60,20 +60,28 @@ const AuthForm = ({ type }: { type: string }) => {
               <CustomInput control={form.control} name="email" label="Email" placeholder="Enter your email" />
               <CustomInput control={form.control} name="password" label="Password" placeholder="Enter your password" />
 
-              <Button type="submit" className="form-btn">
-                {isLoading ? (
-                  <>
-                    <Loader2 size={20} className="animate-spin" />
-                    &nbsp; Loading...
-                  </>
-                ) : type === 'sign-in' ? (
-                  'Sign In'
-                ) : (
-                  'Sign Up'
-                )}
-              </Button>
+              <div className="flex flex-col gap-4">
+                <Button type="submit" className="form-btn" disabled={isLoading}>
+                  {isLoading ? (
+                    <>
+                      <Loader2 size={20} className="animate-spin" />
+                      &nbsp; Loading...
+                    </>
+                  ) : type === 'sign-in' ? (
+                    'Sign In'
+                  ) : (
+                    'Sign Up'
+                  )}
+                </Button>
+              </div>
             </form>
           </Form>
+          <footer className="flex justify-center gap-1">
+            <p className="text-14 font-normal text-gray-600">{type === 'sign-in' ? "Don't have an account?" : 'Already have an account?'}</p>
+            <Link href={type === 'sign-in' ? '/sign-up' : 'sign-in'} className="form-link">
+              {type === 'sign-in' ? 'Sign Up' : 'Sign In'}
+            </Link>
+          </footer>
         </>
       )}
     </section>
